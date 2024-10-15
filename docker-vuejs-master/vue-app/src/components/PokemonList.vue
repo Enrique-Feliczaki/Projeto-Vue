0e9dd41415fd5add771ec421771679143c94b7dc
Enrique-Feliczaki <enrique.zwetsch@gmail.com>
2024-10-15 09:03:43 -0300

    Aprimorando a parte da lista dos pokemons

--- docker-vuejs-master/vue-app/src/components/PokemonList.vue
+++ docker-vuejs-master/vue-app/src/components/PokemonList.vue
@@ -1,36 +1,57 @@
 <template>
     <div>
         <h1>Pokémon</h1>
-        <div v-for="pokemon in pokemons" :key="pokemon.name">
-            <button @click="selectPokemon(pokemon.name)">{{ pokemon.name }}</button>        
-        </div>
-        <button @click="loadMore">Carregar Mais</button>
+        <div class="pokemon-list">
+            <div v-for="pokemon in pokemons" :key="pokemon.name" class="pokemon-item">
+                <button @click="selectPokemon(pokemon.name)">{{ pokemon.name }}</button>        
+            </div>
+         </div>
+        <Pagination :currentPage = "currentPage" :totalPages = "totalPages" @changePage="changePage"/>
     </div>
-
 </template>
+
 <script>
     import {fetchPokemons} from '../api';
+    import Pagination from './Pagination.vue';
+
     export default{
+        components:{Pagination},
         data(){
             return{
                 pokemons:[],
                 offset: 0,
-                limit:20
+                limit:20,
+                currentPage: 1,
+                totalPages: 0,
             };
         },
         async created(){
-            this.pokemons = await fetchPokemons(this.offset,thsi.limit);
+            await this.loadPokemons();
         },
         
         methods:{
-            async loadMore(){
-                this.offset += this.limit;
-                const newPokemon = await fetchPokemons(this.offset,this.limit);
-                this.pokemons.push(...newPokemon);
+            async loadPokemons(){
+                this.pokemons = await fetchPokemons(this.offset, this.limit)
+                this.totalPages = Math.ceil(1118/this.limit);
             },
             selectPokemon(name){
                 this.$emit('select', name);
+            },
+            changePage(page){
+                this.currentPage = page;
+                this.offset = (page - 1) * this.limit;
+                this.loadPokemons();
             }
         }
     };
 </script>
+
+<style scoped>
+    pokemon-list{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    pokemon-item{
+        margin: 10px;
+    }
+</style>
